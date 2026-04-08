00bcb5e565fa32981f54ca96a9d71afa1fa9196d
Samuel Legrain <samuel.legrain@bstorm.be>
2026-04-08 12:17:13 +0200

    Demo_part01

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'grid',
+    loadComponent: () => import('./pages/grid/grid.page').then( m => m.GridPage)
+  },
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, ViewChild } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons],
 })
 export class HomePage {
+  @ViewChild(IonContent) content! : IonContent;
   constructor() {}
+
+  public onClickBottom() : void{
+    this.content.scrollToBottom(500);
+  }
+  
+  public onClickTop() : void{
+    this.content.scrollToTop(500);    
+  }
+
+  public onScroll(event : any) : void{
+    console.log(event);
+  }
 }
